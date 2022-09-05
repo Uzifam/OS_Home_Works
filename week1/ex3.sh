90fa6e5fabe5fd97f380a7acd842d77bd94d96d7
Uzifam <nikita.fedorovich2003@gmail.com>
2022-09-05 15:50:45 +0300

    Change Third Task

--- week1/ex3.sh
+++ week1/ex3.sh
@@ -1,10 +1,9 @@
-date
-sleep 3
 mkdir root
 cd root
 date
 sleep 3
 ls -lt -r / > root.txt
+cat root.txt
 cd -
 date 
 sleep 3
@@ -13,3 +12,6 @@ cd home
 date 
 sleep 3
 ls -lt -r ~/ > home.txt
+cat home.txt
+date
+sleep 3

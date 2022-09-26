e48dd177fff6c74323033d11558cf4e60ec8b96a
Uzifam <nikita.fedorovich2003@gmail.com>
2022-09-26 20:49:01 +0300

    The first Task

---
+++ week4/ex1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main ( void ) {
+    int CommonAncestorPid = getpid();
+//    printf("MAIN PID:%d\n", CommonAncestorPid);
+    fork();
+    fork();
+    clock_t t = clock();
+    int ParentPid = getppid(), currentPid = getpid();
+    if(currentPid != CommonAncestorPid && ParentPid != CommonAncestorPid){
+        kill(currentPid, SIGKILL);
+    }
+    printf("PID:%d     Parent PID:%d\nTime:%ld\n\n", currentPid, ParentPid, t);
+    return EXIT_SUCCESS;
+}
+

1be053cfbb23306cd585f2db626ada0c6c284b96
Uzifam <nikita.fedorovich2003@gmail.com>
2022-11-15 23:36:26 +0300

    The Third Task

---
+++ week10/ex3.sh
@@ -0,0 +1,9 @@
+touch _ex3.txt
+echo "Nikita_Fed" > _ex3.txt
+chmod 666 _ex3.txt
+ls -l _ex3.txt > ex3.txt
+chmod 707 _ex3.txt
+ls -l _ex3.txt >> ex3.txt
+chmod 770 _ex3.txt
+ls -l _ex3.txt >> ex3.txt
+
